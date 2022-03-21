597aea701e81e17decabd566e6d8993436ce0481
Clayton Safranek <claytonsafranek@gmail.com>
2022-03-20 21:22:33 -0500

    Added bulk logic and webpages to app
    Still need to do styling and such

--- app.py
+++ app.py
@@ -1,37 +1,58 @@
-from flask import Flask, render_template, redirect, request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, redirect, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-#configure mysql connection
-app.config['MYSQL_HOST'] = 'ec2-44-202-119-23.compute-1.amazonaws.com'
-app.config['MYSQL_USER'] = 'remoteuser'
-app.config['MYSQL_PASSWORD'] = '@ranD0m1!'
-app.config['MYSQL_DB'] = 'testDB'
-mysql = MySQL(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = "secret"
+db = SQLAlchemy(app)
 
+#db model
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, name, ingredients, description):
+        self.name = name
+        self.ingredients = ingredients
+        self.description = description
+
+#create database
+db.create_all()
 
 #variables to alter between local and production environments
-local = '/'
-prod = '/hello_flask.html'
+local_home = '/index.html'
+prod_home = '/hello_flask.html'
 
-@app.route(local)
+@app.route(local_home, methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html')
-
-@app.route('/info', methods = ['POST', 'GET'])
-def getInfo():
-    if request.method == 'GET':
-        return redirect('/')
-    
+    #return render_template('index.html')
+    #User hit 'Add' to add a new recipe
     if request.method == 'POST':
         name = request.form['name']
-        age = request.form['age']
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO testTable (NAME, AGE) VALUES(%s,%s)''', (name, age))
-        mysql.connection.commit()
-        cursor.close()
-        return "Done!"
+        ingredients = request.form['ingredients']
+        description = request.form['description']
+
+        toAdd = Recipe(name, ingredients, description)
+        db.session.add(toAdd)
+        db.session.commit()
+        return render_template("single_recipe.html", recipe = toAdd)
+
+    #user did not hit submit, just render the home page 
+    return render_template('index.html')
+
+
+
+@app.route('/show_recipes.html')
+def showRecipes():
+    return render_template("show_recipes.html", recipes = Recipe.query.all())
+    
+    
+
+        
 
 
 
